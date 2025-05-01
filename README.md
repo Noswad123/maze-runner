fc4e4b385490eaa7a6f6986570d7ebbd28289c45
Jamal Dawson <Jamal.Dawson.@modernatx.com>
2025-05-01 08:33:07 -0400

    rearrange files

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Run
+# Maze Runner
+
+## What is Maze Runner?
+
+A python app the generates a maze then tries to solve it
+
+## Todo
+- Understand the code
+- Extend the app to do ...
+
+## Run
 
 python3 main.py
 
---
+++ src/cell.py
@@ -0,0 +1,64 @@
+from .graphics import Line, Point, Window
+from .colors import BACKGROUND_COLOR, MOVE_COLOR, FILL_COLOR
+
+class Cell:
+    def __init__(self, win:Window | None=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.visited = False
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, BACKGROUND_COLOR)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, BACKGROUND_COLOR)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, BACKGROUND_COLOR)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, BACKGROUND_COLOR)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = MOVE_COLOR
+        if undo:
+            fill_color = FILL_COLOR
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
+
